ddc44917bf8667801f017f139ba93d03c8b60599
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-14 20:21:33 -0300

    Update prova_14062024.py

--- prova_14062024.py
+++ prova_14062024.py
@@ -3,15 +3,12 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-st.header("prova")
+st.header("Dados da Empresa")
  
 arquivo = "https://github.com/LeticiaAndrade0802/Prova/blob/main/empresa1.csv" 
 dfe = pd.read_csv(arquivo, sep=';') 
-st.dataframe(dfe.head(3))
-
-arquivo = "projetos.csv" 
-df = pd.read_csv(arquivo, sep=';') 
 st.head(23)
+
 
 df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
 df = pd.concat([df, df1])
