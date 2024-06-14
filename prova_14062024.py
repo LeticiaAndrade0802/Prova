5389c716ecaac69f83db50c33e241ca04e9ba846
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-14 20:28:04 -0300

    Update prova_14062024.py

--- prova_14062024.py
+++ prova_14062024.py
@@ -12,13 +12,13 @@ st.dfe(head(23))
 
 df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
 df = pd.concat([df, df1])
-st(df.tail())
+st(df1.tail())
 
 fig, ax = colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
 st.groupby('ano')[colunas].sum()
 st.pyplot(fig)
 
-fig, ax = df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*')
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*')
 st.pyplot.show()
 
 df["Projeto1"].plot(kind = 'hist')
