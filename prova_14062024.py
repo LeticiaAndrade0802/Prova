92687ca0c383c17d1eb1ef9b32109b22b21be03c
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-14 20:31:01 -0300

    Update prova_14062024.py

--- prova_14062024.py
+++ prova_14062024.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.header("Dados da Empresa")
  
-arquivo = "https://raw.githubusercontent.com/LeticiaAndrade0802/aula_teste-/main/projetos.csv" 
+arquivo = "https://raw.githubusercontent.com/LeticiaAndrade0802/prova/main/projetos.csv" 
 dfe = pd.read_csv(arquivo, sep=';') 
 st.dfe(head(23))
 
