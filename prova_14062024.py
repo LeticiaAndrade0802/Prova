529c96195de9d744eae332465cb53b9b0dced33c
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-14 19:51:40 -0300

    Update prova_14062024.py

--- prova_14062024.py
+++ prova_14062024.py
@@ -2,23 +2,40 @@ import pandas as pd
 import ipeadatapy as ip
 import streamlit as st
  
-st.header("Dados da empresa 14 06 2024")
- 
+st.header("Dados da empresa")
+
+
+arquivo = "projetos.csv" 
+df = pd.read_csv(arquivo, sep=';') 
+st.head(23)
+
+df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
+df = pd.concat([df, df1])
+st(df.tail())
+
+colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
+df.groupby('ano')[colunas].sum()
+
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*')
+plt.show()
+
+df["Projeto1"].plot(kind = 'hist')
+df["Projeto4"].plot(kind = 'hist')
+plt.show()
+
+ip.list_series('Selic')
+
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+selic
+
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))")
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))")
+plt.show()
+
 arquivo = "https://raw.githubusercontent.com/LeticiaAndrade0802/prova/main/empresa1.csv" 
 dfe = pd.read_csv(arquivo, sep=';') 
 st.dataframe(dfe.head(3))
  
-fig, ax = plt.subplots()
-dfe.plot(ax=ax)
-st.pyplot(fig)
- 
-fig, ax = plt.subplots()
-dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional',ax=ax)
-st.pyplot(fig)
- 
-fig, ax = plt.subplots()
-dfe["Lucro do período"].plot(kind = 'hist')
-st.pyplot(fig)
- 
+
  
 st.write(dfe.groupby('Ano').mean())
