d40a4622ccf053ffff77d66824f273a6f05e2154
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-14 20:13:50 -0300

    Update prova_14062024.py

--- prova_14062024.py
+++ prova_14062024.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-st.header("Dados da empresa 1")
+st.header("prova")
  
 arquivo = "https://github.com/LeticiaAndrade0802/Prova/blob/main/empresa1.csv" 
 dfe = pd.read_csv(arquivo, sep=';') 
 st.dataframe(dfe.head(3))
 
-arquivo = "projetos.csv" 
+arquivo = "projetos.py" 
 df = pd.read_csv(arquivo, sep=';') 
 st.head(23)
 
@@ -19,5 +19,21 @@ st(df.tail())
 
 colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
 st.groupby('ano')[colunas].sum()
+
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*')
+plt.show()
+
+df["Projeto1"].plot(kind = 'hist')
+df["Projeto4"].plot(kind = 'hist')
+plt.show()
+
+ip.list_series('Selic')
+
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+selic
+
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))")
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))")
+plt.show()
 
 
